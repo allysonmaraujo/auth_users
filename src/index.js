9389a48814184b4ef4ecf22a37af38b7fb768e6a
Allyson Araujo <allyson.m.araujo@gmail.com>
2023-11-29 10:30:50 -0300

    feat: initial projetct structure

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const app = require("./server/server");
+require("dotenv").config();
+
+const port = process.env.SERVER_PORT || 3000;
+
+app.listen(port, () => {
+	console.log(`Servidor subiu na porta: ${port}`);
+});
---
+++ src/server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const routes = require("../routes/routes");
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+module.exports = app;
