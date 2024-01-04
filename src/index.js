f51f7af21959808a2881cd046d645ae9ae5a45d9
Allyson Araujo <allyson.m.araujo@gmail.com>
2024-01-04 19:47:35 -0400

    swagger implementado

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const port = process.env.SERVER_PORT || 3000;
 
+
 app.listen(port, () => {
 	console.log(`Servidor subiu na porta: ${port}`);
 });
--- src/server/server.js
+++ src/server/server.js
@@ -3,8 +3,13 @@ const app = express();
 const cors = require("cors");
 const routes = require("../routes/routes");
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../doc/swagger-output.json');
+
+
 app.use(express.json());
 app.use(cors());
 app.use(routes);
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 module.exports = app;
